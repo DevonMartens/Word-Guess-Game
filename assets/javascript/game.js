2c40ec7a0e8f375b91fab2ecc0aad1cf5fc48d9e
DevonMartens <dmmartens91@gmail.com>
2020-03-24 18:28:07 -0400

    added more variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,9 +5,16 @@ var letterChoice = ['a','b','c','d', 'e','f','g','h','i','j','k','l', 'm', 'n', 
 //word choice
 var words = ['coronavirus', 'diversity', 'respect'];
 
+//var random word
 var randomWord = words[Math.floor(Math.random()*word.length)];
 
+var win = 0;
+var loose = 0;
+var guessesLeft = 0;
+var spaces = '_';
+
 function startGame(){
-for (var i = 0; i <randomWord.length; i++){
-    console.log([i])
-if (letterChoice === )
+var guessWord = randomWord.split('')
+   for (var i = 0; i < guessWord.length; i++)
+}
+if (iser)
