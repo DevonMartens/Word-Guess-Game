6e6a871de0332bfc314dcc1488d1580b9e0b8eb5
DevonMartens <dmmartens91@gmail.com>
2020-03-24 18:10:31 -0400

    started coding this no pseduo code

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+//variable to choose letters
+var letterChoice = ['a','b','c','d', 'e','f','g','h','i','j','k','l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v' , 'w' , 'x', 'y', 'z'];
+
+
+//word choice
+var words = ['coronavirus', 'diversity', 'respect'];
+
+var randomWord = words[Math.floor(Math.random()*word.length)];
+
+function startGame(){
+for (var i = 0; i <randomWord.length; i++){
+    console.log([i])
+if (letterChoice === )
